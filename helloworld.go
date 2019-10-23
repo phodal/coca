edf1115eb6b5c95b30a416fd1764b3cc99c7c9ea
Phodal HUANG <h@phodal.com>
2019-10-23 21:56:04 +0800

    feat: add for loop

--- helloworld.go
+++ helloworld.go
@@ -9,6 +9,13 @@ import (
 )
 
 func main() {
+	var a [3]int // array of 3 integers
+
+	// Print the elements only.
+	for _, v := range a {
+		fmt.Printf("%d\n", v)
+	}
+
 	imp.Count(1)
 	fmt.Println(strings.Join(os.Args[1:], " "))
 }

1609c04a4534a48f2dff157222a2ae30c832cc2e
Phodal HUANG <h@phodal.com>
2019-10-23 22:19:18 +0800

    feat: add cli for test

---
+++ cmd/root.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"fmt"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Used for flags.
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "phocli",
+		Short: "A generator for Cobra based Applications",
+		Long:  `phocli`,
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+
+	fmt.Println(rootCmd.Flag("author").Value.String())
+	return rootCmd.Execute()
+
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
+	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+	viper.SetDefault("license", "apache")
+
+	//rootCmd.AddCommand(addCmd)
+	//rootCmd.AddCommand(initCmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			//er(err)
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- helloworld.go
+++ helloworld.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
-	"./imp"
+	"./cmd"
 )
 
 func main() {
-	var a [3]int // array of 3 integers
-
-	// Print the elements only.
-	for _, v := range a {
-		fmt.Printf("%d\n", v)
-	}
-
-	imp.Count(1)
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	cmd.Execute()
 }

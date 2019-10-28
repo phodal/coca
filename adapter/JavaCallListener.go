73cd4331bb35d68636de795bbd16ed2bfc19a7e2
Phodal HUANG <h@phodal.com>
2019-10-28 16:44:51 +0800

    feat: add convert tree

--- adapter/JavaCallListener.go
+++ adapter/JavaCallListener.go
@@ -4,6 +4,7 @@ import (
 	. "../language/java"
 	. "./models"
 	"fmt"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
 
@@ -11,6 +12,7 @@ var imports []string
 var currentPkg = ""
 var currentClz = ""
 var methodCalls []JMethodCall
+var currentMethodCall *JMethodCall;
 
 type JavaCallListener struct {
 	BaseJavaParserListener
@@ -36,14 +38,30 @@ func (s *JavaCallListener) EnterInterfaceDeclaration(ctx *InterfaceDeclarationCo
 
 func (s *JavaCallListener) EnterInterfaceMethodDeclaration(ctx *InterfaceMethodDeclarationContext) {
 	methodName := ctx.IDENTIFIER().GetText()
-	currentMethodCall := &JMethodCall{currentPkg, currentClz, methodName}
+	currentMethodCall = &JMethodCall{currentPkg, currentClz, methodName}
 	methodCalls = append(methodCalls, *currentMethodCall)
 }
 
 func (s *JavaCallListener) EnterMethodDeclaration(ctx *MethodDeclarationContext) {
 	methodName := ctx.IDENTIFIER().GetText()
-	currentMethodCall := &JMethodCall{currentPkg, currentClz, methodName}
+	currentMethodCall = &JMethodCall{currentPkg, currentClz, methodName}
 	methodCalls = append(methodCalls, *currentMethodCall)
+}
 
-	fmt.Println(methodCalls)
+func (s *JavaCallListener) EnterMethodCall(ctx *MethodCallContext) {
+	if currentMethodCall != nil {
+		var targetType = parseTargetType(ctx);
+		callee := ctx.GetChild(0).(antlr.ParseTree).GetText()
+
+		fmt.Println(targetType, callee)
+	}
+}
+
+
+func parseTargetType(ctx *MethodCallContext) string {
+	var targetCtx antlr.ParseTree = ctx.GetParent().GetChild(0).(antlr.ParseTree)
+	targetVar := targetCtx.GetText();
+	targetType := targetVar;
+
+	return targetType
 }

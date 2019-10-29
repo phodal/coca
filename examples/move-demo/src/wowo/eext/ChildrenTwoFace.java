95e8085d819f5152a9a3b8281869fc626736584c
Phodal HUANG <h@phodal.com>
2019-10-29 17:34:39 +0800

    test: add demo

--- examples/move-demo/src/wowo/eext/ChildrenTwoFace.java
+++ examples/move-demo/src/wowo/eext/ChildrenTwoFace.java
@@ -1,7 +1,10 @@
 package wowo.eext;
 
 import wowo.eext.sub.ChildFace;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+import java.util.concurrent.ExecutionException;
 
 public class ChildrenTwoFace extends ChildFace {
-    
+
 }

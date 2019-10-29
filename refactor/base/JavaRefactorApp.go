cd5083db1d501103c184b40bf8e8c1f4bb4a6ce9
Phodal HUANG <h@phodal.com>
2019-10-29 12:47:25 +0800

    feat: move fiels

--- refactor/base/JavaRefactorApp.go
+++ refactor/base/JavaRefactorApp.go
@@ -3,6 +3,7 @@ package base
 import (
 	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -45,15 +46,27 @@ func handleNode() {
 	var imports map[string]JImport = node.GetImports()
 
 	fmt.Println(node.Pkg+"."+node.Name, imports, node.GetMethods(), fields)
-
 	if len(fields) == 0 {
-		removeAllImports(imports)
+		//removeAllImports(imports)
+		return
 	}
 
-	for index := range fields {
-		field := fields[index]
-		fmt.Println(field)
-	}
+	//for index := range fields {
+		//field := fields[index]
+		//errorLine := 0
+		//
+		//for index := range imports {
+		//	imp := imports[index]
+		//	ss := strings.Split(imp.Name, ".")
+		//	lastField := ss[len(ss)-1]
+		//
+		//	if (lastField == field.Name) {
+		//		continue
+		//	} else {
+		//
+		//	}
+		//}
+	//}
 }
 
 func removeAllImports(imports map[string]JImport) {
@@ -64,7 +77,21 @@ func removeAllImports(imports map[string]JImport) {
 }
 
 func removeImportByLineNum(imp JImport) {
-	fmt.Println(currentFile, imp)
+	removeLine(currentFile, imp.StartLine - 1)
+}
+
+func removeLine(path string, lineNumber int) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	info, _ := os.Stat(path)
+	mode := info.Mode()
+
+	array := strings.Split(string(file), "\n")
+	array = append(array[:lineNumber], array[lineNumber+1:]...)
+	_ = ioutil.WriteFile(path, []byte(strings.Join(array, "\n")), mode)
 }
 
 func (j *JavaRefactorApp) javaFiles(codeDir string) []string {

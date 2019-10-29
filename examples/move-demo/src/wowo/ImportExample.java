b7825724927584d9adbc9d780fc98021a33dcaec
Phodal HUANG <h@phodal.com>
2019-10-29 15:30:42 +0800

    feat: update demos

--- examples/move-demo/src/wowo/ImportExample.java
+++ examples/move-demo/src/wowo/ImportExample.java
@@ -1,7 +1,13 @@
 package wowo;
 
 import wowo.b.PlaceHolder;
+import wowo.a.OriginExample;
 
 public class ImportExample {
     private PlaceHolder placeHolder;
+    private OriginExample originExample;
+
+    public ImportExample(PlaceHolder placeHolder) {
+        this.placeHolder = placeHolder;
+    }
 }

42204985e94ccb1b8373cc248bf3382fbad53981
Phodal HUANG <h@phodal.com>
2019-10-30 11:43:22 +0800

    docs: reanem config from config file to suffix .coca

--- refactor/main.go
+++ refactor/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	rename := NewRemoveMethodApp("configs/move.config", "examples/move-demo/src/")
+	rename := NewRemoveMethodApp("configs/move.coca", "examples/move-demo/src/")
 	rename.Analysis()
 
-	app2 := NewRemoveUnusedImportApp("configs/move.config", "examples/move-demo/src/")
+	app2 := NewRemoveUnusedImportApp("configs/move.coca", "examples/move-demo/src/")
 	app2.Analysis()
 }

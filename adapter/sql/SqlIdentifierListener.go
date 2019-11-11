a81d2424b22bc441222a94a8100556394cbaa5cc
Phodal Huang <h@phodal.com>
2019-11-11 13:52:50 +0800

    feat: ad basic sql parser

--- adapter/sql/SqlIdentifierListener.go
+++ adapter/sql/SqlIdentifierListener.go
@@ -13,6 +13,14 @@ func NewSqlIdentifierListener() *SqlIdentifierListener {
 	return &SqlIdentifierListener{}
 }
 
-func (s *SqlIdentifierListener) EnterCompound_select_stmt(ctx *Compound_select_stmtContext) {
+func (s *SqlIdentifierListener) EnterSelect_stmt(ctx *Select_stmtContext) {
+
+}
+
+func (s *SqlIdentifierListener) EnterSelect_or_values(ctx *Select_or_valuesContext) {
+
+}
+
+func (s *SqlIdentifierListener) EnterSelect_core(ctx *Select_coreContext) {
 	fmt.Println(ctx.GetText())
 }

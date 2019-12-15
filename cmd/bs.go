2772fb890904ede023fad639cf146e9cbf1e6492
Phodal Huang <h@phodal.com>
2019-12-15 17:34:42 +0800

    feat: add bs code

---
+++ cmd/bs.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"coca/src/bs"
+	"coca/src/utils"
+	"encoding/json"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+var badsmellCmd *cobra.Command = &cobra.Command{
+	Use:   "bs",
+	Short: "Bad Code Smell",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		importPath := cmd.Flag("path").Value.String()
+		ignoreStr := cmd.Flag("ignore").Value.String()
+		sortType := cmd.Flag("sort").Value.String()
+
+		ignoreRules := strings.Split(ignoreStr, ",")
+
+		if importPath != "" {
+			bsApp := new(bs.BadSmellApp)
+			bsList := bsApp.AnalysisPath(importPath, ignoreRules)
+
+			bsModel, _ := json.MarshalIndent(bsList, "", "\t")
+
+			if sortType == "type" {
+				sortSmells := sortSmellByType(bsList)
+				bsModel, _ = json.MarshalIndent(sortSmells, "", "\t")
+			}
+
+			utils.WriteToFile("bs.json", string(bsModel))
+		}
+	},
+}
+
+func sortSmellByType(models []bs.BadSmellModel) map[string][]bs.BadSmellModel {
+	sortSmells := make(map[string][]bs.BadSmellModel)
+	for _, model := range models {
+		sortSmells[model.Bs] = append(sortSmells[model.Bs], model)
+	}
+
+	return sortSmells
+}
+
+func init() {
+	rootCmd.AddCommand(badsmellCmd)
+
+	badsmellCmd.PersistentFlags().StringP("path", "p", "", "example -p src/main")
+	badsmellCmd.PersistentFlags().StringP("ignore", "x", "", "-x=dataClass,lazyElement,longMethod,refusedBequest")
+	badsmellCmd.PersistentFlags().StringP("sort", "s", "", "sort bad smell -s=type")
+}
---
+++ src/bs/models/BsModel.go
@@ -0,0 +1,65 @@
+package models
+
+type JFullClassNode struct {
+	Package     string
+	Class       string
+	Type        string
+	Path        string
+	Extends     string
+	Implements  []string
+	Methods     []JFullMethod
+	MethodCalls []JFullMethodCall
+	ClassBS     ClassBadSmellInfo
+}
+
+type JFullMethodCall struct {
+	Package           string
+	Type              string
+	Class             string
+	MethodName        string
+	StartLine         int
+	StartLinePosition int
+	StopLine          int
+	StopLinePosition  int
+}
+
+type JFullMethod struct {
+	Name              string
+	Type              string
+	StartLine         int
+	StartLinePosition int
+	StopLine          int
+	StopLinePosition  int
+	MethodBody        string
+	Parameters        []JFullParameter
+	MethodBs          MethodBadSmellInfo
+}
+
+type MethodBadSmellInfo struct {
+	IfSize     int
+	SwitchSize int
+}
+
+type ClassBadSmellInfo struct {
+	OverrideSize  int
+	PublicVarSize int
+}
+
+type JFullParameter struct {
+	Name string
+	Type string
+}
+
+func NewJFullClassNode() *JFullClassNode {
+	info := &ClassBadSmellInfo{0, 0};
+	return &JFullClassNode{
+		"",
+		"",
+		"",
+		"",
+		"",
+		nil,
+		nil,
+		nil,
+		*info}
+}

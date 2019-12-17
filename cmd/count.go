c206c624a59f81f5f3d0bf0fa0eaf7c0736d3594
Phodal Huang <h@phodal.com>
2019-12-17 13:22:24 +0800

    feat: add basic count logic

--- cmd/count.go
+++ cmd/count.go
@@ -1,25 +1,57 @@
 package cmd
 
 import (
+	"coca/src/models"
+	"coca/src/support"
+	"encoding/json"
+	"fmt"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 type CountCmdConfig struct {
-	Refs bool
+	Refs           bool
+	DependencePath string
 }
 
 var (
 	countCmdConfig CountCmdConfig
 )
 
+var cparsedDeps []models.JClassNode
+
 var countCmd *cobra.Command = &cobra.Command{
 	Use:   "count",
 	Short: "count code",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		dependence := countCmdConfig.DependencePath
+		if dependence == "" {
+			return
+		}
 
-		if countCmdConfig.Refs {
+		file := support.ReadFile(dependence)
+		if file == nil {
+			log.Fatal("lost file:" + dependence)
+		}
 
+		_ = json.Unmarshal(file, &cparsedDeps)
+
+		var callMap = make(map[string]int)
+		for _, clz := range cparsedDeps {
+			for _, call := range clz.MethodCalls {
+				if callMap[call.Package+"."+call.Class+"."+call.MethodName] == 0 {
+					callMap[call.Package+"."+call.Class+"."+call.MethodName] = 1
+				} else {
+					callMap[call.Package+"."+call.Class+"."+call.MethodName]++
+				}
+			}
+		}
+
+		callMapSort := support.RankByWordCount(callMap)
+
+		for _, count := range callMapSort {
+			fmt.Println(count.Value, count.Key)
 		}
 	},
 }
@@ -27,5 +59,6 @@ var countCmd *cobra.Command = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(countCmd)
 
-	countCmd.PersistentFlags().BoolVarP(&countCmdConfig.Refs, "refs", "s", false, "count refs")
+	countCmd.PersistentFlags().StringVarP(&countCmdConfig.DependencePath, "dependence", "d", "", "get dependence file")
+	//countCmd.PersistentFlags().BoolVarP(&countCmdConfig.Refs, "refs", "s", false, "count refs")
 }

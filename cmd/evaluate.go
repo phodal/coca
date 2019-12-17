8e4ba71598adafd078357784de98bcf0cde6c67e
Phodal Huang <h@phodal.com>
2019-12-17 23:02:44 +0800

    feat: thinking in testable

---
+++ cmd/evaluate.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"coca/config"
+	"coca/core/domain/concept"
+	"coca/core/domain/evaluate"
+	. "coca/core/support"
+	"encoding/json"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var evaluateCmd *cobra.Command = &cobra.Command{
+	Use:   "concept",
+	Short: "concept api",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		dependence := cmd.Flag("dependence").Value.String()
+
+		if dependence != "" {
+			analyser := evaluate.NewEvaluateAnalyser()
+			file := ReadFile(dependence)
+			if file == nil {
+				log.Fatal("lost file:" + dependence)
+			}
+
+			_ = json.Unmarshal(file, &parsedDeps)
+
+			analyser.Analysis(&parsedDeps)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(evaluateCmd)
+
+	evaluateCmd.PersistentFlags().StringP("dependence", "d", config.CocaConfig.ReporterPath+"/deps.json", "get dependence file")
+}
---
+++ core/domain/evaluate/analyser.go
@@ -0,0 +1,17 @@
+package evaluate
+
+import "coca/core/models"
+
+type EvaluateAnalyser struct {
+
+}
+
+
+func NewEvaluateAnalyser() EvaluateAnalyser {
+	return *&EvaluateAnalyser{}
+}
+
+
+func (a EvaluateAnalyser) Analysis(nodes *[]models.JClassNode) {
+
+}

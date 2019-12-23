2e2c7466efe109e3ff0d52de69e765dc9fdaca18
Phodal Huang <h@phodal.com>
2019-12-23 20:42:24 +0800

    feat: thinking in evaluate

--- core/domain/evaluate/evaluator/service.go
+++ core/domain/evaluate/evaluator/service.go
@@ -1,13 +1,39 @@
 package evaluator
 
 import (
-	"fmt"
 	"github.com/phodal/coca/core/models"
 )
 
 type Service struct {
 }
 
-func (Service) Evaluate(models.JClassNode) {
-	fmt.Println("Service")
+func (s Service) Evaluate(models.JClassNode) {
+	if s.hasLifeCycle() {
+
+	}
+	if s.hasSameBehavior() {
+
+	}
+	if s.hasModelLike() {
+
+	}
+	if s.hasSameReturnType() {
+
+	}
+}
+
+func (s Service) hasLifeCycle() bool {
+	return false
+}
+
+func (s Service) hasSameBehavior() bool {
+	return false
+}
+
+func (s Service) hasModelLike() bool {
+	return false
+}
+
+func (s Service) hasSameReturnType() bool {
+	return false
 }

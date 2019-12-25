ea34e11a18a2f0bd9b7005459de13bcc2f6d257c
Phodal Huang <h@phodal.com>
2019-12-25 13:01:28 +0800

    refactoring: extract count

--- cmd/count.go
+++ cmd/count.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/olekukonko/tablewriter"
 	"github.com/phodal/coca/config"
+	"github.com/phodal/coca/core/domain/count"
 	"github.com/phodal/coca/core/models"
 	"github.com/phodal/coca/core/support"
 	"github.com/spf13/cobra"
@@ -40,32 +41,12 @@ var countCmd = &cobra.Command{
 
 		_ = json.Unmarshal(file, &cparsedDeps)
 
-		var projectMethods = make(map[string]bool)
-		for _, clz := range cparsedDeps {
-			for _, method := range clz.Methods {
-				projectMethods[clz.Package+"."+clz.Class+"."+method.Name] = true
-			}
-		}
-
-		// TODO: support identify data class
-		var callMap = make(map[string]int)
-		for _, clz := range cparsedDeps {
-			for _, call := range clz.MethodCalls {
-				callMethod := call.Package + "." + call.Class + "." + call.MethodName
-				if projectMethods[callMethod] {
-					if callMap[callMethod] == 0 {
-						callMap[callMethod] = 1
-					} else {
-						callMap[callMethod]++
-					}
-				}
-			}
-		}
+		callMap := count.BuildCallMap(cparsedDeps)
 
 		callMapSort := support.RankByWordCount(callMap)
 
-		if *&countCmdConfig.Top > 0 {
-			callMapSort = callMapSort[:*&countCmdConfig.Top]
+		if countCmdConfig.Top > 0 {
+			callMapSort = callMapSort[:countCmdConfig.Top]
 		}
 
 		table := tablewriter.NewWriter(os.Stdout)

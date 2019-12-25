da4bd44bcd70467c59d50f65e7f111b459e1d563
Phodal Huang <h@phodal.com>
2019-12-26 07:33:55 +0800

    refactor: extract vars

--- core/adapter/call/JavaCallApp_test.go
+++ core/adapter/call/JavaCallApp_test.go
@@ -28,6 +28,11 @@ func TestJavaCallListener_EnterConstructorDeclaration(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	codePath := "../../../_fixtures/suggest/factory"
+	callNodes := getCallNodes(codePath)
+	g.Expect(len(callNodes[0].Methods)).To(Equal(3))
+}
+
+func getCallNodes(codePath string) []models.JClassNode {
 	identifierApp := new(identifier.JavaIdentifierApp)
 	iNodes := identifierApp.AnalysisPath(codePath)
 	var classes []string = nil
@@ -38,23 +43,14 @@ func TestJavaCallListener_EnterConstructorDeclaration(t *testing.T) {
 	callApp := NewJavaCallApp()
 
 	callNodes := callApp.AnalysisPath(codePath, classes, iNodes)
-	g.Expect(len(callNodes[0].Methods)).To(Equal(3))
+	return callNodes
 }
 
 func TestLambda_Express(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	codePath := "../../../_fixtures/lambda"
-	identifierApp := new(identifier.JavaIdentifierApp)
-	iNodes := identifierApp.AnalysisPath(codePath)
-	var classes []string = nil
-	for _, node := range iNodes {
-		classes = append(classes, node.Package+"."+node.ClassName)
-	}
-
-	callApp := NewJavaCallApp()
-
-	callNodes := callApp.AnalysisPath(codePath, classes, iNodes)
+	callNodes := getCallNodes(codePath)
 
 	methodMap := make(map[string]models.JMethod)
 	for _, c := range callNodes[1].Methods {

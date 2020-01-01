4852578ea39025abbf52e9ec07cfa66e3ae5f79d
Phodal Huang <h@phodal.com>
2020-01-01 15:02:10 +0800

    poc: [wasm] make analysis works

--- wasm/main.go
+++ wasm/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/phodal/coca/core/adapter/coca_file"
 	"github.com/phodal/coca/core/domain"
+	"github.com/phodal/coca/core/infrastructure/ast/full"
 	"github.com/phodal/coca/core/infrastructure/ast/identifier"
 	"syscall/js"
 )
@@ -30,14 +32,42 @@ func CompileCodeCallback(value js.Value, args []js.Value) interface{} {
 	return nil
 }
 
-func CompileCode(code string) []domain.JIdentifier {
+func CompileCode(code string) []domain.JClassNode {
+	classes, identMap := prepareForAnalysis(code)
+
+	parser := coca_file.ProcessString(code)
+	context := parser.CompilationUnit()
+
+	listener := full.NewJavaFullListener(identMap, "hello")
+	listener.AppendClasses(classes)
+
+	antlr.NewParseTreeWalker().Walk(listener, context)
+
+	nodes := listener.GetNodeInfo()
+	fmt.Println(nodes)
+
+	return nodes
+
+}
+
+func prepareForAnalysis(code string) ([]string, map[string]domain.JIdentifier) {
 	parser := coca_file.ProcessString(code)
 	context := parser.CompilationUnit()
 
 	listener := identifier.NewJavaIdentifierListener()
-
 	antlr.NewParseTreeWalker().Walk(listener, context)
 
 	identifiers := listener.GetNodes()
-	return identifiers
+
+	var classes []string = nil
+
+	for _, node := range identifiers {
+		classes = append(classes, node.Package+"."+node.ClassName)
+	}
+
+	var identMap = make(map[string]domain.JIdentifier)
+	for _, ident := range identifiers {
+		identMap[ident.GetClassFullName()] = ident
+	}
+	return classes, identMap
 }

b5005ae20d91d8d6bafc837aa59e27bf908d2edd
Phodal Huang <h@phodal.com>
2020-01-01 14:38:28 +0800

    feat: make wasm works

--- wasm/main.go
+++ wasm/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/phodal/coca/core/adapter/coca_file"
+	"github.com/phodal/coca/core/domain"
 	"github.com/phodal/coca/core/infrastructure/ast/identifier"
+	"syscall/js"
 )
 
-func main()  {
-	parser := coca_file.ProcessString(`
-package com.phodal.coca.analysis.identifier.model;
-
-public class DataClass {
-    private String date;
-
-    public String getDate() {
-        return date;
-    }
+func registerCallbacks() {
+	js.Global().Set("compileCode", js.FuncOf(CompileCodeCallback))
 }
-`)
+
+func main() {
+	c := make(chan struct{}, 0)
+	registerCallbacks()
+	<-c
+}
+
+func CompileCodeCallback(value js.Value, args []js.Value) interface{} {
+	callback := args[len(args)-1:][0]
+	message := args[0].String()
+
+	results := CompileCode(message)
+
+	identModel, _ := json.Marshal(results)
+	callback.Invoke(js.Null(), string(identModel))
+	return nil
+}
+
+func CompileCode(code string) []domain.JIdentifier {
+	parser := coca_file.ProcessString(code)
 	context := parser.CompilationUnit()
 
 	listener := identifier.NewJavaIdentifierListener()
@@ -26,5 +39,5 @@ public class DataClass {
 	antlr.NewParseTreeWalker().Walk(listener, context)
 
 	identifiers := listener.GetNodes()
-	fmt.Println(identifiers)
+	return identifiers
 }

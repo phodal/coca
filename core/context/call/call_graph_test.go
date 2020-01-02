1eab89b405832de9bbfe110ebd2c23182238d06b
Phodal Huang <h@phodal.com>
2020-01-02 10:02:02 +0800

    test: add test for call api graph

--- core/context/call/call_graph_test.go
+++ core/context/call/call_graph_test.go
@@ -2,16 +2,18 @@ package call_test
 
 import (
 	"encoding/json"
+	"fmt"
 	. "github.com/onsi/gomega"
 	"github.com/phodal/coca/cmd/cmd_util"
+	"github.com/phodal/coca/core/context/analysis"
+	"github.com/phodal/coca/core/context/api"
 	"github.com/phodal/coca/core/context/call"
 	"github.com/phodal/coca/core/domain"
 	"path/filepath"
 	"testing"
 )
 
-
-func Test_should_generate_correct_files(t *testing.T) {
+func Test_ShouldBuildSuccessDataFromJson(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	var parsedDeps []domain.JClassNode
@@ -31,6 +33,78 @@ func Test_should_generate_correct_files(t *testing.T) {
 "com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.BookRepository.save";
 "com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.model.Book.getId";
 "com.phodal.pholedge.book.BookController.createBook" -> "com.phodal.pholedge.book.BookService.createBook";
+}
+`))
+
+}
+
+func Test_ShouldBuildSuccessDataFromSourceData(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+
+	codePath := "../../../_fixtures/examples/api"
+	identApp := analysis.NewJavaIdentifierApp()
+	identifiers := identApp.AnalysisPath(filepath.FromSlash(codePath))
+	var classes []string = nil
+	for _, node := range identifiers {
+		classes = append(classes, node.Package+"."+node.ClassName)
+	}
+	callApp := analysis.NewJavaFullApp()
+	callNodes := callApp.AnalysisPath(codePath, classes, identifiers)
+	identifiersMap := domain.BuildIdentifierMap(identifiers)
+	diMap := domain.BuildDIMap(identifiers, identifiersMap)
+	app := new(api.JavaApiApp)
+	restApis := app.AnalysisPath(codePath, callNodes, identifiersMap, diMap)
+
+	analyser := call.NewCallGraph()
+	dotContent, apis := analyser.AnalysisByFiles(restApis, callNodes, diMap)
+
+	g.Expect(len(apis)).To(Equal(4))
+
+	g.Expect(dotContent).To(Equal(`digraph G { 
+
+"POST /books" -> "com.phodal.pholedge.book.BookController.createBook";
+"com.phodal.pholedge.book.BookFactory.create" -> "com.phodal.pholedge.core.IdGenerator.generate";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder().id";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder().id(id).isbn";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder().id(id).isbn(isbn).name";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder().id(id).isbn(isbn).name(name).createdAt";
+"com.phodal.pholedge.book.model.Book.create" -> "com.phodal.pholedge.book.model.Book.builder().id(id).isbn(isbn).name(name).createdAt(now()).build";
+"com.phodal.pholedge.book.BookFactory.create" -> "com.phodal.pholedge.book.model.Book.create";
+"com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.BookFactory.create";
+"com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.model.command.CreateBookCommand.getIsbn";
+"com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.model.command.CreateBookCommand.getName";
+"com.phodal.pholedge.book.BookRepository.save" -> "com.phodal.pholedge.book.model.this.bookMapper.doSave";
+"com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.BookRepository.save";
+"com.phodal.pholedge.book.BookService.createBook" -> "com.phodal.pholedge.book.model.Book.getId";
+"com.phodal.pholedge.book.BookController.createBook" -> "com.phodal.pholedge.book.BookService.createBook";
+
+"PUT /books/{id}" -> "com.phodal.pholedge.book.BookController.updateBook";
+"com.phodal.pholedge.book.BookRepository.byId" -> "com.phodal.pholedge.book.model.this.bookMapper.byId";
+"com.phodal.pholedge.book.BookRepository.byId" -> "com.phodal.pholedge.core.exception.NotFoundException.";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.BookRepository.byId";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.model.Book.save";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.model.command.UpdateBookCommand.getIsbn";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.model.command.UpdateBookCommand.getName";
+"com.phodal.pholedge.book.BookRepository.save" -> "com.phodal.pholedge.book.model.this.bookMapper.doSave";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.BookRepository.save";
+"com.phodal.pholedge.book.model.Book.toRepresentation" -> "com.phodal.pholedge.book.model.BookRepresentaion.";
+"com.phodal.pholedge.book.BookService.updateBook" -> "com.phodal.pholedge.book.model.Book.toRepresentation";
+"com.phodal.pholedge.book.BookController.updateBook" -> "com.phodal.pholedge.book.BookService.updateBook";
+
+"GET /books/" -> "com.phodal.pholedge.book.BookController.getBookList";
+"com.phodal.pholedge.book.BookRepository.list" -> "com.phodal.pholedge.book.model.this.bookMapper.list";
+"com.phodal.pholedge.book.BookService.getBooksLists" -> "com.phodal.pholedge.book.BookRepository.list";
+"com.phodal.pholedge.book.BookController.getBookList" -> "com.phodal.pholedge.book.BookService.getBooksLists";
+
+"GET /books/{id}" -> "com.phodal.pholedge.book.BookController.getBookById";
+"com.phodal.pholedge.book.BookRepository.byId" -> "com.phodal.pholedge.book.model.this.bookMapper.byId";
+"com.phodal.pholedge.book.BookRepository.byId" -> "com.phodal.pholedge.core.exception.NotFoundException.";
+"com.phodal.pholedge.book.BookService.getBookById" -> "com.phodal.pholedge.book.BookRepository.byId";
+"com.phodal.pholedge.book.model.Book.toRepresentation" -> "com.phodal.pholedge.book.model.BookRepresentaion.";
+"com.phodal.pholedge.book.BookService.getBookById" -> "com.phodal.pholedge.book.model.Book.toRepresentation";
+"com.phodal.pholedge.book.BookController.getBookById" -> "com.phodal.pholedge.book.BookService.getBookById";
 }
 `))
 

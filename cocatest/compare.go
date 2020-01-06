109c12d788317c2ba0b45053410d47f072c9aa09
Phodal Huang <h@phodal.com>
2020-01-06 11:41:05 +0800

    test: comments unused code

--- cocatest/compare.go
+++ cocatest/compare.go
@@ -2,7 +2,6 @@ package cocatest
 
 import (
 	"encoding/json"
-	"io"
 	"reflect"
 )
 
@@ -18,16 +17,16 @@ func JSONBytesEqual(a, b []byte) (bool, error) {
 	return reflect.DeepEqual(j2, j), nil
 }
 
-func JSONEqual(a, b io.Reader) (bool, error) {
-	var j, j2 interface{}
-	d := json.NewDecoder(a)
-	if err := d.Decode(&j); err != nil {
-		return false, err
-	}
-	d = json.NewDecoder(b)
-	if err := d.Decode(&j2); err != nil {
-		return false, err
-	}
-	return reflect.DeepEqual(j2, j), nil
-}
-
+//func JSONEqual(a, b io.Reader) (bool, error) {
+//	var j, j2 interface{}
+//	d := json.NewDecoder(a)
+//	if err := d.Decode(&j); err != nil {
+//		return false, err
+//	}
+//	d = json.NewDecoder(b)
+//	if err := d.Decode(&j2); err != nil {
+//		return false, err
+//	}
+//	return reflect.DeepEqual(j2, j), nil
+//}
+//

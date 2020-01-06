543e09f50fa65dfa56621be33ebf1e927a08bc70
Phodal Huang <h@phodal.com>
2020-01-06 11:09:32 +0800

    feat: init jdep model

---
+++ core/domain/jdependency.go
@@ -0,0 +1,21 @@
+package domain
+
+type JDependency struct {
+	GroupId    string
+	ArtifactId string
+	Scope      string
+	Type       string
+	Version    string
+	Optional   bool
+}
+
+func NewJDependency(group string, artifact string) *JDependency {
+	return &JDependency{
+		GroupId:    group,
+		ArtifactId: artifact,
+		Scope:      "",
+		Type:       "",
+		Version:    "",
+		Optional:   false,
+	}
+}

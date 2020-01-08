2f58502769aaf682cc8bd5488e17d94a51c1699a
Phodal Huang <h@phodal.com>
2020-01-08 11:19:15 +0800

    test: add test data

---
+++ _fixtures/deps/maven_sample/src/main/java/com/phodal/coca/depanalysis/DepanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.phodal.coca.depanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepanalysisApplication.class, args);
+	}
+
+}
---
+++ _fixtures/deps/maven_sample/src/test/java/com/phodal/coca/depanalysis/DepanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phodal.coca.depanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

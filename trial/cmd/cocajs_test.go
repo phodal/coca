867c7fb6c691b0a331d02b48a3eac4c0248e7de3
Phodal Huang <h@phodal.com>
2020-01-09 14:30:59 +0800

    test: [cocajs] add first test

---
+++ trial/cmd/cocajs_test.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"github.com/phodal/coca/cocatest"
+	"github.com/phodal/coca/cocatest/testcase"
+	"testing"
+)
+
+func RunTrialTestCmd(t *testing.T, tests []testcase.CmdTestCase) {
+	cocatest.RunTestCaseWithCmd(t, tests, NewTrialRootCmd)
+}
+
---
+++ trial/cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/phodal/coca/cmd/config"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	versionCmd.SetOut(output)
+	trialRootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(output, "Coca Version: " + config.VERSION + " -- HEAD")
+	},
+}

9a4c84e2c297d2e590f459ae7e75b0b7459ed3ba
Phodal Huang <h@phodal.com>
2020-01-10 14:18:54 +0800

    test: [ts] add import examples files

---
+++ _fixtures/ts/Import.ts
@@ -0,0 +1,5 @@
+import { ZipCodeValidator } from "./ZipCodeValidator";
+import * as validator from "./ZipCodeValidator";
+import "./my-module.js";
+import $ from "JQuery";
+import zip = require("./ZipCodeValidator");

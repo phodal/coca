bcc345ee188cf6692b2dd0b9adcdc08751c30f4a
Phodal Huang <h@phodal.com>
2020-01-11 13:57:01 +0800

    bug: add regression bug

---
+++ _fixtures/ts/regressions/catch_comment_issue.ts
@@ -0,0 +1,13 @@
+import bluebird from "bluebird";
+import mongoose from "mongoose";
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
+});
---
+++ trial/pkg/application/ts/ts_ident_app_regressioon_test.go
@@ -0,0 +1,19 @@
+package ts
+
+import (
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"testing"
+)
+
+
+func Test_Regression(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	app := new(TypeScriptApiApp)
+	code, _ := ioutil.ReadFile("../../../../_fixtures/ts/regressions/catch_comment_issue.ts")
+
+	results := app.Analysis(string(code), "")
+
+	g.Expect(len(results.Imports)).To(Equal(1))
+}

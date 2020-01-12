fcb8bcf5dafd338a9df328629bcc0a3053e8debe
Phodal Huang <h@phodal.com>
2020-01-12 23:19:29 +0800

    poc: add type

--- trial/cocago/parser.go
+++ trial/cocago/parser.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/token"
 	"io/ioutil"
+	"reflect"
 )
 
 type WalkFunc func(ast.Node) (ast.Node, bool)
@@ -19,14 +20,27 @@ func ProcessFile(fileName string) {
 		panic(err)
 	}
 
+	Visitor(f, fset)
+}
+
+func Visitor(f *ast.File, fset *token.FileSet) {
 	ast.Inspect(f, func(n ast.Node) bool {
 		var s string
 		switch x := n.(type) {
+		case *ast.File:
+			//packageName := x.Name
 		case *ast.Ident:
 			s = x.Name
+		case *ast.TypeSpec:
+			fmt.Println(x.Name)
+		case *ast.StructType:
+			fmt.Println(reflect.TypeOf(x))
+			for _, field := range x.Fields.List {
+				fmt.Println(field.Names, field.Type)
+			}
 		}
 		if s != "" {
-			fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
+			//fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
 		}
 		return true
 	})

2676f66113452f302ec63bf77d44311083054db7
Phodal Huang <h@phodal.com>
2020-01-12 19:22:33 +0800

    test: remove code for better temple string issue debug

--- _fixtures/ts/regressions/template_string_issues.ts
+++ _fixtures/ts/regressions/template_string_issues.ts
@@ -1,10 +1,4 @@
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "/auth/facebook/callback",
-    profileFields: ["name", "email", "link", "locale", "timezone"],
-    passReqToCallback: true
-}, (req: any, accessToken, refreshToken, profile, done) => {
+passport.use(new FacebookStrategy({}, (req: any, accessToken, refreshToken, profile, done) => {
     if (req.user) {
         User.findOne({ facebook: profile.id }, (err, existingUser) => {
             if (err) { return done(err); }
@@ -25,32 +19,6 @@ passport.use(new FacebookStrategy({
                     });
                 });
             }
-        });
-    } else {
-        User.findOne({ facebook: profile.id }, (err, existingUser) => {
-            if (err) { return done(err); }
-            if (existingUser) {
-                return done(undefined, existingUser);
-            }
-            User.findOne({ email: profile._json.email }, (err, existingEmailUser) => {
-                if (err) { return done(err); }
-                if (existingEmailUser) {
-                    req.flash("errors", { msg: "There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings." });
-                    done(err);
-                } else {
-                    const user: any = new User();
-                    user.email = profile._json.email;
-                    user.facebook = profile.id;
-                    user.tokens.push({ kind: "facebook", accessToken });
-                    user.profile.name = `${profile.name.givenName} ${profile.name.familyName}`;
-                    user.profile.gender = profile._json.gender;
-                    user.profile.picture = `https://graph.facebook.com/${profile.id}/picture?type=large`;
-                    user.profile.location = (profile._json.location) ? profile._json.location.name : "";
-                    user.save((err: Error) => {
-                        done(err, user);
-                    });
-                }
-            });
         });
     }
 }));

081671e5aeb5ef14c1ac23db7cf388160f0ca5b1
Phodal Huang <h@phodal.com>
2020-01-12 11:20:23 +0800

    fix: fix import issues

--- trial/pkg/application/ts/ts_ident_app_regressioon_test.go
+++ trial/pkg/application/ts/ts_ident_app_regressioon_test.go
@@ -10,7 +10,7 @@ func Test_Regression(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	app := new(TypeScriptApiApp)
-	code, _ := ioutil.ReadFile("../../../../_fixtures/ts/regressions/catch_comment_issue.ts")
+	code, _ := ioutil.ReadFile("../../../../_fixtures/ts/regressions/import_comma_issue.ts")
 
 	results := app.Analysis(string(code), "")
 

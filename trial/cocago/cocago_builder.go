32693ffbf7d2df7ec532174593679100caa3da0c
Phodal Huang <h@phodal.com>
2020-01-14 14:20:20 +0800

    refactor: move builder

--- trial/cocago/cocago_builder.go
+++ trial/cocago/cocago_builder.go
@@ -1,20 +1,82 @@
 package cocago
 
 import (
+	"fmt"
 	"github.com/phodal/coca/pkg/domain/trial"
 	"go/ast"
+	"reflect"
 )
 
-func AddStructType(currentStruct trial.CodeDataStruct, x *ast.StructType, currentFile *trial.CodeFile) {
-	member := trial.CodeMember{
-		DataStructID: currentStruct.Name,
-		Type:         "struct",
+func BuildPropertyField(name string, field *ast.Field) *trial.CodeProperty {
+	var typeName string
+	var typeType string
+	switch x := field.Type.(type) {
+	case *ast.Ident:
+		typeType = "Identify"
+		typeName = x.String()
+	case *ast.ArrayType:
+		typeType = "ArrayType"
+		switch typeX := x.Elt.(type) {
+		case *ast.Ident:
+			typeName = typeX.String()
+		case *ast.SelectorExpr:
+			typeName = getSelectorName(*typeX)
+		default:
+			fmt.Println("BuildPropertyField ArrayType", reflect.TypeOf(x.Elt))
+		}
+	case *ast.FuncType:
+		typeType = "Function"
+		typeName = "func"
+	case *ast.StarExpr:
+		typeName = getStarExprName(*x)
+		typeType = "Star"
+	case *ast.SelectorExpr:
+		typeName = getSelectorName(*x)
+	default:
+		fmt.Println("BuildPropertyField", reflect.TypeOf(x))
 	}
-	for _, field := range x.Fields.List {
-		property := BuildPropertyField(getFieldName(field), field)
-		member.FileID = currentFile.FullName
-		currentStruct.Properties = append(currentStruct.Properties, *property)
+
+	property := &trial.CodeProperty{
+		Modifiers: nil,
+		Name:      name,
+		TypeType:  typeType,
+		TypeName:  typeName,
 	}
-	currentFile.Members = append(currentFile.Members, &member)
-	currentFile.DataStructures = append(currentFile.DataStructures, currentStruct)
+	return property
 }
+
+func getSelectorName(typeX ast.SelectorExpr) string {
+	return typeX.X.(*ast.Ident).String() + "." + typeX.Sel.Name
+}
+
+func getStarExprName(starExpr ast.StarExpr) string {
+	switch x := starExpr.X.(type) {
+	case *ast.Ident:
+		return x.Name
+	case *ast.SelectorExpr:
+		return getSelectorName(*x)
+	default:
+		fmt.Println("getStarExprName", reflect.TypeOf(x))
+		return ""
+	}
+}
+
+func BuildFunction(x *ast.FuncDecl) *trial.CodeFunction {
+	codeFunc := &trial.CodeFunction{
+		Name: x.Name.String(),
+	}
+
+	if x.Type.Params != nil {
+		codeFunc.Parameters = append(codeFunc.Parameters, BuildFieldToProperty(x.Type.Params.List)...)
+	}
+
+	if x.Type.Results != nil {
+		codeFunc.ReturnTypes = append(codeFunc.Parameters, BuildFieldToProperty(x.Type.Results.List)...)
+	}
+
+	for _, item := range x.Body.List {
+		BuildMethodCall(codeFunc, item)
+	}
+	return codeFunc
+}
+
--- trial/cocago/cocago_parser.go
+++ trial/cocago/cocago_parser.go
@@ -152,37 +152,6 @@ func BuildReceiver(x *ast.FuncDecl, recv string) string {
 	return recv
 }
 
-func getStarExprName(starExpr ast.StarExpr) string {
-	switch x := starExpr.X.(type) {
-	case *ast.Ident:
-		return x.Name
-	case *ast.SelectorExpr:
-		return getSelectorName(*x)
-	default:
-		fmt.Println("getStarExprName", reflect.TypeOf(x))
-		return ""
-	}
-}
-
-func BuildFunction(x *ast.FuncDecl) *trial.CodeFunction {
-	codeFunc := &trial.CodeFunction{
-		Name: x.Name.String(),
-	}
-
-	if x.Type.Params != nil {
-		codeFunc.Parameters = append(codeFunc.Parameters, BuildFieldToProperty(x.Type.Params.List)...)
-	}
-
-	if x.Type.Results != nil {
-		codeFunc.ReturnTypes = append(codeFunc.Parameters, BuildFieldToProperty(x.Type.Results.List)...)
-	}
-
-	for _, item := range x.Body.List {
-		BuildMethodCall(codeFunc, item)
-	}
-	return codeFunc
-}
-
 func BuildMethodCall(codeFunc *trial.CodeFunction, item ast.Stmt) {
 	switch it := item.(type) {
 	case *ast.ExprStmt:
@@ -272,44 +241,17 @@ func getFieldName(field *ast.Field) string {
 	return field.Names[0].Name
 }
 
-func BuildPropertyField(name string, field *ast.Field) *trial.CodeProperty {
-	var typeName string
-	var typeType string
-	switch x := field.Type.(type) {
-	case *ast.Ident:
-		typeType = "Identify"
-		typeName = x.String()
-	case *ast.ArrayType:
-		typeType = "ArrayType"
-		switch typeX := x.Elt.(type) {
-		case *ast.Ident:
-			typeName = typeX.String()
-		case *ast.SelectorExpr:
-			typeName = getSelectorName(*typeX)
-		default:
-			fmt.Println("BuildPropertyField ArrayType", reflect.TypeOf(x.Elt))
-		}
-	case *ast.FuncType:
-		typeType = "Function"
-		typeName = "func"
-	case *ast.StarExpr:
-		typeName = getStarExprName(*x)
-		typeType = "Star"
-	case *ast.SelectorExpr:
-		typeName = getSelectorName(*x)
-	default:
-		fmt.Println("BuildPropertyField", reflect.TypeOf(x))
+func AddStructType(currentStruct trial.CodeDataStruct, x *ast.StructType, currentFile *trial.CodeFile) {
+	member := trial.CodeMember{
+		DataStructID: currentStruct.Name,
+		Type:         "struct",
 	}
-
-	property := &trial.CodeProperty{
-		Modifiers: nil,
-		Name:      name,
-		TypeType:  typeType,
-		TypeName:  typeName,
+	for _, field := range x.Fields.List {
+		property := BuildPropertyField(getFieldName(field), field)
+		member.FileID = currentFile.FullName
+		currentStruct.Properties = append(currentStruct.Properties, *property)
 	}
-	return property
+	currentFile.Members = append(currentFile.Members, &member)
+	currentFile.DataStructures = append(currentFile.DataStructures, currentStruct)
 }
 
-func getSelectorName(typeX ast.SelectorExpr) string {
-	return typeX.X.(*ast.Ident).String() + "." + typeX.Sel.Name
-}

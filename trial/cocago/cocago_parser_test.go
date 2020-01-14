9a7e734eff0a9eeb999434cb750fa5c1a62dc6ec
Phodal Huang <h@phodal.com>
2020-01-14 14:50:17 +0800

    refactor: make same testing

--- trial/cocago/cocago_parser_test.go
+++ trial/cocago/cocago_parser_test.go
@@ -28,37 +28,36 @@ func Test_DataStructProperty(t *testing.T) {
 	t.Parallel()
 	g := NewGomegaWithT(t)
 
-	results := testParser.ProcessFile("testdata/data_struct_property.code")
-	g.Expect(len(results.Members)).To(Equal(1))
-	properties := results.DataStructures[0].Properties
-
-	g.Expect(len(properties)).To(Equal(5))
-	g.Expect(properties[0].Name).To(Equal("FullName"))
-	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/data_struct_property.json")).To(Equal(true))
+	var test = "data_struct_property"
+	results := testParser.ProcessFile("testdata/" + test + ".code")
+	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/" + test + ".json")).To(Equal(true))
 }
 
 func Test_DataStructWithFuncType(t *testing.T) {
 	t.Parallel()
 	g := NewGomegaWithT(t)
 
-	results := testParser.ProcessFile("testdata/struct_with_func.code")
-	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/struct_with_func.json")).To(Equal(true))
+	var test = "struct_with_func"
+	results := testParser.ProcessFile("testdata/" + test + ".code")
+	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/" + test + ".json")).To(Equal(true))
 }
 
 func Test_DataStructWithFuncDecl(t *testing.T) {
 	t.Parallel()
 	g := NewGomegaWithT(t)
 
-	results := testParser.ProcessFile("testdata/struct_with_func_decl.code")
-	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/struct_with_func_decl.json")).To(Equal(true))
+	var test = "struct_with_func_decl"
+	results := testParser.ProcessFile("testdata/" + test + ".code")
+	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/" + test + ".json")).To(Equal(true))
 }
 
 func Test_DataStructZero(t *testing.T) {
 	t.Parallel()
 	g := NewGomegaWithT(t)
 
-	results := testParser.ProcessFile("testdata/struct_type_zero.code")
-	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/struct_type_zero.json")).To(Equal(true))
+	var test = "struct_type_zero"
+	results := testParser.ProcessFile("testdata/" + test + ".code")
+	g.Expect(cocatest.JSONFileBytesEqual(results, "testdata/" + test + ".json")).To(Equal(true))
 }
 
 func Test_NormalMethod(t *testing.T) {

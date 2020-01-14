460ebbe1c920953f9bce18866fce3f8fa6f45e35
Phodal Huang <h@phodal.com>
2020-01-14 21:49:16 +0800

    fix: fix buffer not init issues

--- languages/python/python_base_lexer.go
+++ languages/python/python_base_lexer.go
@@ -1,16 +1,17 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/phodal/coca/pkg/infrastructure/container"
 )
 
 var TabSize = 8
 var indents *container.Stack
+var buffer []antlr.Token
 
 func init() {
 	indents = container.NewStack()
+	buffer = make([]antlr.Token, 32)
 }
 
 type PythonBaseLexer struct {
@@ -19,7 +20,6 @@ type PythonBaseLexer struct {
 	firstTokenIndex int
 	lastTokenIndex  int
 	opened          int
-	buffer          []antlr.Token
 	lastToken       antlr.Token
 }
 
@@ -37,21 +37,21 @@ func (l *PythonBaseLexer) BuildTokenByType(tokenIndex int, channel int, text str
 func (l *PythonBaseLexer) EmitToken(token antlr.Token) {
 	l.BaseLexer.EmitToken(token)
 
-	if l.buffer[l.firstTokenIndex] != nil {
+	if buffer[l.firstTokenIndex] != nil {
 		l.lastTokenIndex = l.IncTokenInd(l.lastTokenIndex)
 
 		if l.firstTokenIndex == l.lastTokenIndex {
-			var newArray = make([]antlr.Token, len(l.buffer)*2)
-			destIndex := len(newArray) - (len(l.buffer) - l.firstTokenIndex)
-			copy(newArray, l.buffer)
-			copy(newArray, l.buffer[:len(l.buffer)-l.firstTokenIndex])
+			var newArray = make([]antlr.Token, len(buffer)*2)
+			destIndex := len(newArray) - (len(buffer) - l.firstTokenIndex)
+			copy(newArray, buffer)
+			copy(newArray, buffer[:len(buffer)-l.firstTokenIndex])
 
 			l.firstTokenIndex = destIndex
-			l.buffer = newArray
+			buffer = newArray
 		}
 	}
 
-	l.buffer[l.lastTokenIndex] = token
+	buffer[l.lastTokenIndex] = token
 	l.lastToken = token
 }
 
@@ -67,7 +67,7 @@ func (l *PythonBaseLexer) DecIndentLevel() {
 
 func (l *PythonBaseLexer) NextToken() antlr.Token {
 	if l.GetInputStream().LA(1) == antlr.TokenEOF && indents.Len() > 0 {
-		if l.buffer[l.lastTokenIndex] == nil || l.buffer[l.lastTokenIndex].GetTokenType() != PythonLexerLINE_BREAK {
+		if buffer[l.lastTokenIndex] == nil || buffer[l.lastTokenIndex].GetTokenType() != PythonLexerLINE_BREAK {
 			l.BaseLexer.EmitToken(l.BuildDefaultToken(PythonLexerLINE_BREAK))
 		}
 
@@ -79,17 +79,12 @@ func (l *PythonBaseLexer) NextToken() antlr.Token {
 
 	next := l.BaseLexer.NextToken()
 
-	fmt.Println(next)
-	if l.buffer == nil {
+	if buffer[l.firstTokenIndex] == nil {
 		return next
 	}
 
-	if l.buffer[l.firstTokenIndex] == nil {
-		return next
-	}
-
-	var result = l.buffer[l.firstTokenIndex]
-	l.buffer[l.firstTokenIndex] = nil
+	var result = buffer[l.firstTokenIndex]
+	buffer[l.firstTokenIndex] = nil
 
 	if l.firstTokenIndex != l.lastTokenIndex {
 		l.firstTokenIndex = l.IncTokenInd(l.firstTokenIndex)
@@ -98,18 +93,17 @@ func (l *PythonBaseLexer) NextToken() antlr.Token {
 	return result
 }
 
-
 func (l *PythonBaseLexer) HandleNewLine() {
 	l.EmitToken(l.BuildTokenByType(PythonLexerNEWLINE, antlr.LexerHidden, l.GetText()))
 
-	next := string(l.GetInputStream().LA(1))
+	next := string(rune(l.GetInputStream().LA(1)))
 	if next != " " && next != "\t" && l.IsNotNewLineOrComment(next) {
 		l.ProcessNewLine(0)
 	}
 }
 
 func (l *PythonBaseLexer) HandleSpaces() {
-	next := string(l.GetInputStream().LA(1))
+	next := string(rune(l.GetInputStream().LA(1)))
 
 	if (l.lastToken == nil || l.lastToken.GetTokenType() == PythonLexerNEWLINE) && l.IsNotNewLineOrComment(next) {
 		indent := 0
@@ -134,7 +128,7 @@ func (l *PythonBaseLexer) IsNotNewLineOrComment(next string) bool {
 }
 
 func (l *PythonBaseLexer) IncTokenInd(index int) int {
-	return (index + 1) % len(l.buffer)
+	return (index + 1) % len(buffer)
 }
 
 func (l *PythonBaseLexer) ProcessNewLine(indent int) {

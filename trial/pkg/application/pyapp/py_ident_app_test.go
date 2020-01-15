ffdcfa60ad7077daf31c03e736e5228acd31f19c
Phodal Huang <h@phodal.com>
2020-01-15 11:28:14 +0800

    test: add python code for grammer test

--- trial/pkg/application/pyapp/py_ident_app_test.go
+++ trial/pkg/application/pyapp/py_ident_app_test.go
@@ -1,11 +1,33 @@
 package pyapp
 
 import (
+	"fmt"
 	. "github.com/onsi/gomega"
+	"github.com/phodal/coca/pkg/adapter/cocafile"
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
+
+func Test_AllPythonGrammar(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	app := new(PythonApiApp)
+
+	var PyFileFilter = func(path string) bool {
+		return strings.HasSuffix(path, ".py")
+	}
+	files := cocafile.GetFilesWithFilter("testdata/grammar/", PyFileFilter)
+
+	for _, file := range files {
+		fmt.Println(file)
+		file, _ := ioutil.ReadFile(file)
+		app.Analysis(string(file), "")
+	}
+
+	g.Expect(1).To(Equal(1))
+}
 
 func Test_PythonClass(t *testing.T) {
 	g := NewGomegaWithT(t)
@@ -17,7 +39,6 @@ func Test_PythonClass(t *testing.T) {
 
 	g.Expect(1).To(Equal(1))
 }
-
 
 func Test_PythonFuncDefStm(t *testing.T) {
 	g := NewGomegaWithT(t)

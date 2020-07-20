56d8e0a153db00dad1eacab011a2f17f4a3ba9f2
Phodal Huang <h@phodal.com>
2020-07-20 16:48:46 +0800

    feat: add count beanch

--- coca.go
+++ coca.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/phodal/coca/cmd"
+	"github.com/pkg/profile"
 	"os"
+	"time"
 )
 
 func main() {
-	//defer profile.Start().Stop()
+	t1 := time.Now() // get current time
+	defer profile.Start().Stop()
 	output := os.Stdout
 	rootCmd := cmd.NewRootCmd(output)
 	_ = rootCmd.Execute()
+	elapsed := time.Since(t1)
+	fmt.Println("App elapsed: ", elapsed)
 }

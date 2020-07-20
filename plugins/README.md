d9493745442cdeefca2b42648c822bba28ce4962
Phodal Huang <h@phodal.com>
2020-07-21 07:43:15 +0800

    refactor: remove plugins code

--- plugins/README.md
+++
@@ -1,5 +0,0 @@
-Goals: Language as Plugins
-
-Stage 1: C++ API for Golang
-
-Stage 2: Rust API for Golang
--- plugins/pgroovy/wrap.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "wrap.h"
-
-using namespace std;
-
-int output()
-{
-    cout << "Hello, World!";
-}

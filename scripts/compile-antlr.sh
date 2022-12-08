00ee6f1adbb01d46378d950b3cb5d97dbc0425ce
Phodal Huang <h@phodal.com>
2022-12-08 17:38:21 +0800

    build: split java parser

--- scripts/compile-antlr.sh
+++ scripts/compile-antlr.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd languages/g4
+cd languages/java
 
-antlr -Dlanguage=Go -listener JavaLexer.g4 -o ../java
-antlr -Dlanguage=Go -listener JavaParser.g4 -o ../java
+antlr -Dlanguage=Go -listener JavaLexer.g4
+antlr -Dlanguage=Go -listener JavaParser.g4
+
+cd ../g4
 
 #antlr -Dlanguage=Go -listener Sql.g4 -o ../sql
 
